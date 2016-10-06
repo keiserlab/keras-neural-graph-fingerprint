3dad465424a1eb0ff2c4072d7bc353b17c321089
Ties van Rozendaal <git@tivaro.nl>
2016-10-06 10:46:10 -0700

    Initial commit: Imported some data and utils scripts from other NGF repositories

---
+++ features.py
@@ -0,0 +1,49 @@
+# Imported from https://github.com/HIPS/neural-fingerprint/blob/master/neuralfingerprint/features.py
+#           and https://github.com/HIPS/neural-fingerprint/blob/master/neuralfingerprint/util.py
+import autograd.numpy as np
+from rdkit import Chem
+
+def one_of_k_encoding(x, allowable_set):
+    if x not in allowable_set:
+        raise Exception("input {0} not in allowable set{1}:".format(x, allowable_set))
+    return map(lambda s: x == s, allowable_set)
+
+def one_of_k_encoding_unk(x, allowable_set):
+    """Maps inputs not in the allowable set to the last element."""
+    if x not in allowable_set:
+        x = allowable_set[-1]
+    return map(lambda s: x == s, allowable_set)
+
+def atom_features(atom):
+    return np.array(one_of_k_encoding_unk(atom.GetSymbol(),
+                                      ['C', 'N', 'O', 'S', 'F', 'Si', 'P', 'Cl', 'Br', 'Mg', 'Na',
+                                       'Ca', 'Fe', 'As', 'Al', 'I', 'B', 'V', 'K', 'Tl', 'Yb',
+                                       'Sb', 'Sn', 'Ag', 'Pd', 'Co', 'Se', 'Ti', 'Zn', 'H',    # H?
+                                       'Li', 'Ge', 'Cu', 'Au', 'Ni', 'Cd', 'In', 'Mn', 'Zr',
+                                       'Cr', 'Pt', 'Hg', 'Pb', 'Unknown']) +
+                    one_of_k_encoding(atom.GetDegree(), [0, 1, 2, 3, 4, 5]) +
+                    one_of_k_encoding_unk(atom.GetTotalNumHs(), [0, 1, 2, 3, 4]) +
+                    one_of_k_encoding_unk(atom.GetImplicitValence(), [0, 1, 2, 3, 4, 5]) +
+                    [atom.GetIsAromatic()])
+
+def bond_features(bond):
+    bt = bond.GetBondType()
+    return np.array([bt == Chem.rdchem.BondType.SINGLE,
+                     bt == Chem.rdchem.BondType.DOUBLE,
+                     bt == Chem.rdchem.BondType.TRIPLE,
+                     bt == Chem.rdchem.BondType.AROMATIC,
+                     bond.GetIsConjugated(),
+                     bond.IsInRing()])
+
+def num_atom_features():
+    # Return length of feature vector using a very simple molecule.
+    m = Chem.MolFromSmiles('CC')
+    alist = m.GetAtoms()
+    a = alist[0]
+    return len(atom_features(a))
+
+def num_bond_features():
+    # Return length of feature vector using a very simple molecule.
+    simple_mol = Chem.MolFromSmiles('CC')
+    Chem.SanitizeMol(simple_mol)
+    return len(bond_features(simple_mol.GetBonds()[0]))
